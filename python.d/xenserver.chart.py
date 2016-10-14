b7f700c7decd9b1ba7eb1f7efa8624928d775955
Romain Dartigues <rdartigues.ext@orange.com>
2016-10-14 17:19:52 +0200

    WIP: save work

--- python.d/xenserver.chart.py
+++ python.d/xenserver.chart.py
@@ -13,6 +13,7 @@ Sample configuration:
 '''
 
 # stdlib
+import random
 import re
 
 # netdata dependency
@@ -43,7 +44,7 @@ ORDER = [
     'VM_metrics',
 ]
 
-CHART = {
+CHARTS = {
     'PIF_metrics': {
         'options': [None, 'Physical InterFaces', 'kiB/s', 'bandwidth', 'pif.metrics'],
         'lines': [
@@ -90,15 +91,34 @@ CHART = {
 
 
 
+
+#dbg_file = open('/tmp/rdartigues.log', 'w', 1)
+dbg_file = __import__('sys').stdout
+dbg = lambda s:(dbg_file.write('%s\n'%(s,)), dbg_file.flush())
+
 class Service(SimpleService):
     session = None
-    url = None
-    username = None
-    password = None
+    ''':class:`XenAPI.Session` instance
+    '''
 
+    url = None
+    '''URL of the XenServer API (without credentials)
+    '''
+
+    username = ''
+    '''username (extracted from the URI)
+    '''
+
+    password = ''
+    '''password (extracted from the URI)
+    '''
 
     def __init__(self, configuration=None, name=None):
         SimpleService.__init__(self, configuration, name)
+
+        self.definitions = CHARTS
+        self.order = sorted(CHARTS)
+
         if self.configuration.get('url'):
             self.url = self.configuration['url']
             match = _uri_credentials.match(self.url)
@@ -108,38 +128,99 @@ class Service(SimpleService):
                 self.url = self.url.replace(match.group('credentials'), '')
 
 
+    def check(self):
+        """
+        Check if service is able to connect to server
+        :return: boolean
+        """
+        dbg('xs: check...')
+        return isinstance(self._connect(), XenAPI.Session)
+
+
+#    def create(self):
+#        dbg('xs: create!')
+#        self.chart("example.python_random", '', 'A random number', 'random number',
+#                   'random', 'random', 'line', self.priority, self.update_every)
+#        self.dimension('random1')
+#        self.commit()
+#        return True
+
+
+#    def update(self, interval):
+#        dbg('xs: update')
+#        self.begin("example.python_random", interval)
+#        self.set("random1", random.randint(0, 100))
+#        self.end()
+#        self.commit()
+#        return True
+
+
     def _connect(self, force=False):
         '''Attempt to connect to a local XenServer API through :attr:`url`
 
         :param bool force: attempt a connection even if one seems present
-        :return: a (hopefully active) XenAPI session
-        :rtype: XenAPI.Session
+        :return: status connection
+        :rtype: bool
         '''
-        if not self.session or force:
+        if XenAPI is None:
+            return False
+
+        if not isinstance(self.session, XenAPI.Session) or force:
             if self.url and (self.username or self.password):
                 self.session = XenAPI.Session(self.url)
-                self.session.login_with_password(
-                    self.username or '',
-                    self.password or '',
-                )
             else:
                 self.session = XenAPI.xapi_local()
-        return self.session
 
+            self.session.login_with_password(
+                self.username or '',
+                self.password or '',
+                '1.0',
+                'netdata',
+            )
 
-    def check(self):
-        if XenAPI is not None:
-            try:
-                return bool(self._connect())
-            except:
-                pass
-        return False
+        return isinstance(self.session, XenAPI.Session)
 
 
     def _get_data(self):
-        if not self.session:
-            try:
-                self._connect()
-            except:
-                return
-        # TODO
+        """
+        Get some data
+        :rtype: dict or None
+        """
+        if not self._connect():
+            return
+        data = {}
+
+        host_ref = self.session.xenapi.session.get_this_host(self.session._session)
+        host = self.session.xenapi.host.get_record(host_ref)
+        host['metrics'] = self.session.xenapi.host_metrics.get_record(host['metrics'])
+
+        # self.session.xenapi.SR.get_all_records()
+
+#        VMs = self.session.xenapi.VM.get_all_records()
+        VMs = self.session.xenapi.VM.get_all_records_where(
+            'field "resident_on" = "%s"' % ( # and field "is_control_domain" = "false"
+                host_ref,
+            )
+        )
+
+        for VM in VMs:
+            VM['metrics'] = self.session.xenapi.VM_metrics.get_record(VM['metrics'])
+
+            if not VM['is_control_domain']:
+                VM['VDIs'] = (
+                    self.get_domU_VDIs_from_name(VM['name_label'])
+                    or
+                    self.get_domU_VDIs_from_conf(VM['name_label'])
+                )
+
+#        VM_metrics = self.session.xenapi.VM_metrics.get_all_records()
+
+        return data
+
+
+
+
+
+if __name__ == '__main__':
+    self = Service({'update_every':3, 'priority':99999, 'retries':3})
+    self.check()
